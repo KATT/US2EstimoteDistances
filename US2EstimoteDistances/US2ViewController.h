d6ef8b83739ca7487e66b627df63122bc5dbccdf
Alexander Johansson <ajax@ustwo.co.uk>
2014-02-13 13:41:25 +0000

    initial

---
+++ US2EstimoteDistances/US2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  US2ViewController.h
+//  US2EstimoteDistances
+//
+//  Created by A on 13/02/2014.
+//  Copyright (c) 2014 ustwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface US2ViewController : UIViewController
+
+@end
